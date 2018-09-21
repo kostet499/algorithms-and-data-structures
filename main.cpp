6c47ff73c53ebebc1a9b387566dd03d87d4a7fab
Toreno <kostet499@gmail.com>
2018-09-21 19:11:12 +0300

    Refactored

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void z_function(string &pattern, vector <int> &z) {
-    z.resize(pattern.length(), 0);
-    int left = 0, right = 0;
+void z_function(const string &pattern, vector<int> &z_values) {
+    z_values.resize(pattern.length(), 0);
+    int left = 0;
+    int right = 0;
     for(int i = 1; i < pattern.length(); i++) {
-        if(i <= right)
-            z[i] = min(z[i - left], right - i + 1);
+        if(i <= right) {
+            z_values[i] = min(z_values[i - left], right - i + 1);
+        }
 
-        while(i + z[i] < pattern.length() && pattern[i + z[i]] == pattern[z[i]])
-            z[i]++;
+        while(i + z_values[i] < pattern.length() && pattern[i + z_values[i]] == pattern[z_values[i]]) {
+            ++z_values[i];
+        }
 
-        if(i + z[i] - 1 > right) {
+        if(i + z_values[i] - 1 > right) {
             left = i;
-            right = i + z[i] - 1;
+            right = i + z_values[i] - 1;
         }
     }
 }
@@ -25,37 +29,44 @@ int main() {
 
     string pattern;
     getline(cin, pattern, '\n');
-    vector <int> z;
+    vector<int> z_values;
     pattern += '#';
     char c;
     int size = pattern.size() - 1;
-    z_function(pattern, z);
+    z_function(pattern, z_values);
 
-    int cur = 0, left = 0, right = 0, emp = size - 1;
-    vector <int> zf(size, 0);
-    vector <char> cf(size);
-    for(int i = 0; i < size - 1; i++)
-        if(!(cin >> cf[i]))
+    int cur = 0, left = 0, right = 0, next = size - 1;
+    vector<int> z_cycle(size, 0);
+    vector<char> char_cycle(size);
+    for(int i = 0; i < size - 1; i++) {
+        if (!(cin >> char_cycle[i])) {
             return 0;
+        }
+    }
 
     int i = 0;
-    while(cin >> cf[emp]) {
-        zf[emp] = 0;
+    while(cin >> char_cycle[next]) {
+        z_cycle[next] = 0;
         cur = i % size;
-        if(i <= right)
-            zf[cur] = min(z[i - left], right - i + 1);
-
-        while(zf[cur] < size && cf[(i + zf[cur]) % size] == pattern[zf[cur]])
-            zf[cur]++;
-
-        if(i + zf[cur] - 1 > right) {
-            left = i;
-            right = i + zf[cur] - 1;
+        if(i <= right) {
+            z_cycle[cur] = min(z_values[i - left], right - i + 1);
         }
-        if(zf[cur] == size)
+
+        while(z_cycle[cur] < size && char_cycle[(i + z_cycle[cur]) % size] == pattern[z_cycle[cur]]) {
+            ++z_cycle[cur];
+        }
+
+        if(i + z_cycle[cur] - 1 > right) {
+            left = i;
+            right = i + z_cycle[cur] - 1;
+        }
+
+        if(z_cycle[cur] == size) {
             cout << i << " ";
-        i++;
-        emp = (emp + 1) % size;
+        }
+
+        ++i;
+        next = (next + 1) % size;
     }
     return 0;
 }
