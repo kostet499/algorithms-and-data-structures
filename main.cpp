3d4406c8036587382fdc1b7fe5e3a6915bbdadc2
Toreno <kostet499@gmail.com>
2018-09-21 17:20:55 +0300

    Prettier code

--- main.cpp
+++ main.cpp
@@ -1,96 +1,105 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
 
 using namespace std;
 
-// 1
-void z_function(string &pattern, vector <int> &z) {
-    z.resize(pattern.length(), 0);
-    int left = 0, right = 0;
+// Counts zet function of the given string
+void z_function(string &pattern, vector<int> &z_values) {
+    z_values.resize(pattern.length(), 0);
+    int left = 0;
+    int right = 0;
     for(int i = 1; i < pattern.length(); i++) {
-        if(i <= right)
-            z[i] = min(z[i - left], right - i + 1);
+        if(i <= right) {
+            z_values[i] = min(z_values[i - left], right - i + 1);
+        }
 
-        while(i + z[i] < pattern.length() && pattern[i + z[i]] == pattern[z[i]])
-            z[i]++;
+        while(i + z_values[i] < pattern.length() && pattern[i + z_values[i]] == pattern[z_values[i]]) {
+            ++z_values[i];
+        }
 
-        if(i + z[i] - 1 > right) {
+        if(i + z_values[i] - 1 > right) {
             left = i;
-            right = i + z[i] - 1;
+            right = i + z_values[i] - 1;
         }
     }
 }
 
-//2
-void prefix_func(string &s, vector <int> &p) {
-    p.resize(s.length(), 0);
-    for(int i = 1; i < p.size(); i++) {
-        int j = p[i - 1];
-        while(j && s[j] != s[i])
-            j = p[j - 1];
+//Counts prefix function of the given string
+void prefix_func(string &pattern, vector<int> &prefix_values) {
+    prefix_values.resize(pattern.length(), 0);
+    for(int i = 1; i < prefix_values.size(); i++) {
+        int j = prefix_values[i - 1];
+        while(j && pattern[j] != pattern[i])
+            j = prefix_values[j - 1];
 
-        if(s[j] == s[i])
-            p[i] = j + 1;
+        if(pattern[j] == pattern[i])
+            prefix_values[i] = j + 1;
     }
 }
 
-//3
-vector <int> z_to_p(vector <int>& z) {
-    vector <int> p(z.size(), 0);
-    z[0] = 0;
-    for(int i = 0, x = 0; i < z.size(); i++, x = i + max(z[i] - 1, 0))
-        p[x] = max(p[x], z[i]);
+// Transforms zet function  to prefix function
+vector<int> z_to_p(vector<int> &z_values) {
+    vector<int> prefix_values(z_values.size(), 0);
+    z_values[0] = 0;
+    for(int i = 0, x = 0; i < z_values.size(); i++) {
+        x = i + max(z_values[i] - 1, 0);
+        prefix_values[x] = max(prefix_values[x], z_values[i]);
+    }
     int cur_m = 0;
-    for(int i = p.size() - 1; i > - 1; i--) {
-        cur_m = max(p[i], cur_m);
-        p[i] = cur_m;
-        cur_m--;
+    for(int i = prefix_values.size() - 1; i > - 1; i--) {
+        cur_m = max(prefix_values[i], cur_m);
+        prefix_values[i] = cur_m;
+        --cur_m;
     }
-    return p;
+    return prefix_values;
 }
 
-//4
-vector <int> p_to_z(vector <int>& p) {
-    vector <int> z(p.size(), 0);
-    for(int i = 0, x = 0; i < p.size(); i++, x = i - max(p[i] - 1, 0))
-        z[x] = max(z[x], p[i]);
-    return z;
+// Transforms prefix function to zet function
+vector<int> p_to_z(vector<int> &prefix_values) {
+    vector<int> z_values(prefix_values.size(), 0);
+    for(int i = 0, x = 0; i < prefix_values.size(); i++) {
+        x = i - max(prefix_values[i] - 1, 0);
+        z_values[x] = max(z_values[x], prefix_values[i]);
+    }
+    return z_values;
 }
 
 // возвращает значение префикс функции для элемента c, если префикс функция элемента до него равна j
-int local_prefix_func(string& s, vector <int> &p, char c, int j) {
-    while(j && c != s[j])
-        j = p[j - 1];
+int local_prefix_func(string &pattern, vector<int> &prefix_values, char c, int last_prefix) {
+    while(last_prefix && c != pattern[last_prefix])
+        last_prefix = prefix_values[last_prefix - 1];
 
-    if(c == s[j])
-        return j + 1;
+    if(c == pattern[last_prefix])
+        return last_prefix + 1;
 
     return 0;
 }
 
-//5
-// просто подбираем символ с подходящим значением префикс-функции
-string min_lex(vector <int>& p) {
-    if(!p.size())
+// Строит лексикографически минимальну строку по заданным значениям префикс фукнции(корректной)
+string min_lex(vector<int> &prefix_values) {
+    if(!prefix_values.size())
         return "";
-    string s = "a";
-    for(int i = 1; i < p.size(); i++) {
+    string minimal_string = "a";
+    for(int i = 1; i < prefix_values.size(); i++) {
         for(int j = 0; j < 26; j++) {
             char c = 'a' + j;
-            if(local_prefix_func(s, p, c, p[i - 1]) == p[i]) {
-                s += c;
+            if(local_prefix_func(minimal_string, prefix_values, c, prefix_values[i - 1]) == prefix_values[i]) {
+                minimal_string += c;
                 break;
             }
         }
     }
-    return s;
+    return minimal_string;
 }
 
 int main() {
-    vector <int> z;
+    vector<int> z_values;
     int x;
     while(cin >> x && x != -1)
-        z.emplace_back(x);
-    z = z_to_p(z);
-    cout << min_lex(z);
+        z_values.emplace_back(x);
+    z_values = z_to_p(z_values);
+    cout << min_lex(z_values);
     return 0;
 }
