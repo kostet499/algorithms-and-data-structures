f410e0fe7ad3ac0f2fcab9d11d31794dc0e66478
Toreno <kostet499@gmail.com>
2018-09-27 19:52:11 +0300

    RefactoredX2

--- main.cpp
+++ main.cpp
@@ -2,23 +2,22 @@
 #include <string>
 #include <iostream>
 
-
 using namespace std;
 
-// Counts zet function of the given string
 void z_function(const string &pattern, vector<int> &z_values) {
     z_values.resize(pattern.length(), 0);
     int left = 0;
     int right = 0;
     for(int i = 1; i < pattern.length(); i++) {
+        // i is inside of the block where we have information about zet function.
         if(i <= right) {
             z_values[i] = min(z_values[i - left], right - i + 1);
         }
-
+        // looking up new symbols.
         while(i + z_values[i] < pattern.length() && pattern[i + z_values[i]] == pattern[z_values[i]]) {
             ++z_values[i];
         }
-
+        // updating block information.
         if(i + z_values[i] - 1 > right) {
             left = i;
             right = i + z_values[i] - 1;
@@ -26,12 +25,11 @@ void z_function(const string &pattern, vector<int> &z_values) {
     }
 }
 
-//Counts prefix function of the given string
 void prefix_func(const string &pattern, vector<int> &prefix_values) {
     prefix_values.resize(pattern.length(), 0);
     for(int i = 1; i < prefix_values.size(); i++) {
         int j = prefix_values[i - 1];
-
+        // jumping back on prefixes of prefixes of prefixes etc.
         while(j && pattern[j] != pattern[i]) {
             j = prefix_values[j - 1];
         }
@@ -42,33 +40,33 @@ void prefix_func(const string &pattern, vector<int> &prefix_values) {
     }
 }
 
-// Transforms zet function  to prefix function
+// Transforms zet function  to prefix function.
 vector<int> z_to_p(const vector<int> &z_values) {
     vector<int> prefix_values(z_values.size(), 0);
-    for(int i = 1, x = 0; i < z_values.size(); i++) {
-        x = i + max(z_values[i] - 1, 0);
-        prefix_values[x] = max(prefix_values[x], z_values[i]);
+    for(int i = 1, prefix_position = 0; i < z_values.size(); i++) {
+        prefix_position = i + max(z_values[i] - 1, 0);
+        prefix_values[prefix_position] = max(prefix_values[prefix_position], z_values[i]);
     }
-    int cur_m = 0;
+    int current_maximum = 0;
     for(int i = prefix_values.size() - 1; i > - 1; i--) {
-        cur_m = max(prefix_values[i], cur_m);
-        prefix_values[i] = cur_m;
-        --cur_m;
+        current_maximum = max(prefix_values[i], current_maximum);
+        prefix_values[i] = current_maximum;
+        --current_maximum;
     }
     return prefix_values;
 }
 
-// Transforms prefix function to zet function
+// Transforms prefix function to zet function.
 vector<int> p_to_z(const vector<int> &prefix_values) {
     vector<int> z_values(prefix_values.size(), 0);
-    for(int i = 0, x = 0; i < prefix_values.size(); i++) {
-        x = i - max(prefix_values[i] - 1, 0);
-        z_values[x] = max(z_values[x], prefix_values[i]);
+    for(int i = 0, z_position = 0; i < prefix_values.size(); i++) {
+        z_position = i - max(prefix_values[i] - 1, 0);
+        z_values[z_position] = max(z_values[z_position], prefix_values[i]);
     }
     return z_values;
 }
 
-// возвращает значение префикс функции для элемента c, если префикс функция элемента до него равна j
+// Returns the value of prefix_function for symbol 'c' if 'last_prefix' is the last prefix function value.
 int local_prefix_func(const string &pattern, const vector<int> &prefix_values, char c, int last_prefix) {
     while(last_prefix && c != pattern[last_prefix]) {
         last_prefix = prefix_values[last_prefix - 1];
@@ -81,7 +79,7 @@ int local_prefix_func(const string &pattern, const vector<int> &prefix_values, c
     return 0;
 }
 
-// Строит лексикографически минимальну строку по заданным значениям префикс фукнции(корректной)
+// Constructs a lexicographically minimal string for the given values of the correct prefix function.
 string min_lex(const vector<int> &prefix_values) {
     if(!prefix_values.size()) {
         return "";
