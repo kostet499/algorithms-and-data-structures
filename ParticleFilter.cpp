39401918eaa0944182fa296d24f07816286e2e10
coolhatsker <kostet499@gmail.com>
2018-11-13 01:30:20 +0300

    Basic concepts formulated

--- ParticleFilter.cpp
+++ ParticleFilter.cpp
@@ -4,5 +4,30 @@
 
 #include "ParticleFilter.h"
 
+state::state(double i, double j, double k) : x(i), y(j), angle(k) {}
+
 ParticleFilter::ParticleFilter(const JsonField &f) : field(f) {}
+
+void ParticleFilter::PassNewVelocity(dot new_velocity) {
+    auto current_time = std::chrono::system_clock::now();
+    std::vector<state> new_particles(particles.size());
+
+    // радиус вектор перемещения в системе отсчёта робота
+    dot shift = motion(current_time);
+    // code ...
+
+    velocity = new_velocity;
+    time = current_time;
+}
+
+
+// зависит от единиц измерения скорости
+// в данный момент предполагаю, что всё в системе СИ
+// можно доработать основываясь на изменении самого вектора скорости от старого к новому
+// пока что я просто умножаю прошлый вектор скорости на прошедшее время
+dot ParticleFilter::motion(std::chrono::_V2::system_clock::time_point current_time, const dot &new_velocity) {
+    auto millisecs = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - time);
+    double time_in_secs = millisecs.count() / 1000.0;
+    return {velocity.x * time_in_secs, velocity.y * time_in_secs};
+}
 
--- ParticleFilter.h
+++ ParticleFilter.h
@@ -2,21 +2,47 @@
 // Created by robocup on 11.11.18.
 //
 
+/*
+ * Мы представляем частицу как координаты центра масс робота x,y
+ * и угол поворота относительно оси, выбранной заранее.
+ *
+ * Также мы оперируем вектором скорости, который измеряется в системе отсчёта робота.
+ * Декартовая система координат робота - ОХ направлена в сторону правого плеча, ОУ перпендикулярна ОХ
+ * и идёт по направлению взгляда робота.  Начало отсчёта - проекция центра масс на плоскость
+ *
+ * Так же, неординарный выбор. Каждый раз в функции PassNewVelocity мы используем для перемещения не новую
+ * скорость, а старую (которая была в предыдущий раз передана в эту функцию). То есть в предыдущий момент мы запомнили
+ * скорость и момент времени передачи этой скорости. Когда мы еще раз функцию, то мы используем старые данные, чтобы
+ * посчитать вектор перемещения и переместиться, и запоминаем новые данные.
+ */
+
 #ifndef PARTICLEFILTER_PARTICLEFILTER_H
 #define PARTICLEFILTER_PARTICLEFILTER_H
 
 #include "JsonField.h"
+#include <vector>
+#include <chrono>
+
+struct state {
+    double x, y;
+    double angle;
+
+    state(double, double, double);
+};
 
 
 class ParticleFilter {
 public:
     explicit ParticleFilter(const JsonField &);
-
+    void PassNewVelocity(dot velocity);
+    void PassNewVision(const std::vector<dot> &objects);
 private:
-    //std::vector<DynamicState> motion_model();
-
+    dot motion(std::chrono::_V2::system_clock::time_point current_time, const dot &new_velocity);
 private:
     JsonField field;
+    std::vector<state> particles;
+    dot velocity;
+    std::chrono::_V2::system_clock::time_point time = std::chrono::system_clock::now();
 };
 
 
