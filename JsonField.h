ff27c45761a9aea5e27dcd516faa179c8d0cb64c
coolhatsker <kostet499@gmail.com>
2018-11-13 00:14:41 +0300

    New approach

---
+++ JsonField.h
@@ -0,0 +1,31 @@
+//
+// Created by robocup on 12.11.18.
+//
+
+#ifndef ALGOS_JSONFIELD_H
+#define ALGOS_JSONFIELD_H
+
+#include "vector"
+
+struct dot {
+    double x;
+    double y;
+
+    dot(double, double);
+    dot();
+};
+
+class JsonField {
+public:
+    explicit JsonField(const char *filename);
+    const dot &operator[](size_t);
+    double width() const;
+    double height() const;
+private:
+    double field_width;
+    double field_height;
+    std::vector<dot> static_field_objects;
+};
+
+
+#endif //ALGOS_JSONFIELD_H
