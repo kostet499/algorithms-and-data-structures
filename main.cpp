b82218d3b2ef4e624ffa4557bef8ace76e455443
coolhatsker <kostet499@gmail.com>
2018-11-17 20:39:39 +0300

    First (and the last) sort added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct point {
+    double x;
+    double y;
+
+    point(double a1, double a2) : x(a1), y(a2){
+
+    }
+
+    point operator+(const point& b) {
+        return {this->x + b.x, this->y + b.y};
+    }
+    point operator-(const point& b) {
+        return {this->x - b.x, this->y - b.y};
+    }
+    bool operator<(const point &b) {
+        return (this->x < b.x) || (this->x == b.x && this->y < b.y);
+    }
+};
+
+void prepare_data(std::vector<point> &data) {
+    double x;
+    double y;
+    while(std::cin >> x) {
+        std::cin >> y;
+        data.emplace_back(point(x, y));
+    }
+    sort(data.begin(), data.end());
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    std::vector<point> dots;
+    prepare_data(dots);
+
     return 0;
 }
