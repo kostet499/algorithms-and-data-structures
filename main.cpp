e5cde3b5bc66928edfe8351434afa22e8ce06c03
coolhatsker <kostet499@gmail.com>
2018-11-19 15:44:11 +0300

    list-release

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <queue>
+#include <list>
+#include <algorithm>
 
 using std::vector;
 
@@ -30,80 +30,70 @@ class ConvexHull {
 public:
     ConvexHull(const vector<point> &point_set) : super_point(0, 0, 0){
         first_initialization(point_set);
-        for(curr_index = 4; curr_index < point_set.size(); ++curr_index) {
+        for(point_index = 4; point_index < point_set.size(); ++point_index) {
             add_point(point_set);
         }
     }
 
     void represent_answer() {
-        std::sort(facet.begin(), facet.end());
-        std::cout << facet.size() - free_facets.size() << std::endl;
-        for(size_t i = 0; i < facet.size(); ++i) {
-            if(facet[i].empty()) {
-                continue;
-            }
-            std::cout << 3 << " " << facet[i][0] << " " << facet[i][1] << " " << facet[i][2] << std::endl;
+        facet.sort();
+        std::cout << facet.size() << std::endl;
+        for(auto &mface : facet) {
+            std::cout << 3 << " " << mface.first[0] << " " << mface.first[1] << " " << mface.first[2] << std::endl;
         }
     }
 private:
-    void sort_facet(size_t facet_id, const vector<point> &point_set) {
-        std::sort(facet[facet_id].begin(), facet[facet_id].end());
-        point ab = point_set[facet[facet_id][1]] - point_set[facet[facet_id][0]];
-        point ac = point_set[facet[facet_id][2]] - point_set[facet[facet_id][0]];
-        if(outside_normal(facet_id, point_set)(ab[ac]) < 0) {
-            std::swap(facet[facet_id][1], facet[facet_id][2]);
+    void sort_facet(vector<size_t> &mface, const vector<point> &point_set) {
+        std::sort(mface.begin(), mface.end());
+        point ab = point_set[mface[1]] - point_set[mface[0]];
+        point ac = point_set[mface[2]] - point_set[mface[0]];
+        if(outside_normal(mface, point_set)(ab[ac]) < 0) {
+            std::swap(mface[1], mface[2]);
         }
     }
 
-    bool is_seen(size_t facet_id, size_t point_id, const vector<point> &point_set) const {
-        return (point_set[point_id] - point_set[facet[facet_id][0]])(outside_normal(facet_id, point_set)) > 0;
+    bool is_seen(vector<size_t> &mface, size_t point_id, const vector<point> &point_set) const {
+        return (point_set[point_id] - point_set[mface[0]])(outside_normal(mface, point_set)) > 0;
     }
 
-    point outside_normal(size_t facet_id, const vector<point> &point_set) const {
-        point ab = point_set[facet[facet_id][1]] - point_set[facet[facet_id][0]];
-        point ac = point_set[facet[facet_id][2]] - point_set[facet[facet_id][0]];
-        return (super_point - point_set[facet[facet_id][0]])(ab[ac]) > 0 ? ac[ab] : ab[ac];
+    point outside_normal(vector<size_t> &mface, const vector<point> &point_set) const {
+        point ab = point_set[mface[1]] - point_set[mface[0]];
+        point ac = point_set[mface[2]] - point_set[mface[0]];
+        return (super_point - point_set[mface[0]])(ab[ac]) > 0 ? ac[ab] : ab[ac];
     }
 
     void add_point(const vector<point> &point_set) {
-        vector<bool> facet_seen(facet.size(), false);
-        for(size_t i = 0; i < facet.size(); ++i) {
-            if(!facet[i].empty() && (facet_seen[i] = is_seen(i, curr_index, point_set))) {
-                facet[i].clear();
-                free_facets.push(i);
+        std::unordered_map<size_t, bool> facet_seen;
+        for(auto mface = facet.begin(); mface != facet.end(); ++mface) {
+            facet_seen[mface->second] = is_seen(mface->first, point_index, point_set);
+            if(facet_seen[mface->second]) {
+                mface = facet.erase(mface);
+                --mface;
             }
         }
 
         std::unordered_map<size_t, size_t> temp;
-        vector<vector<size_t> > new_edges;
-        for(size_t i = 0; i < edges.size(); ++i) {
-            if(edges[i].empty()) {
-                continue;
-            }
-            size_t facet_id1 = edges[i][2];
-            size_t facet_id2 = edges[i][3];
+        std::list<vector<size_t> > new_edges;
+        for(auto medge = edges.begin(); medge != edges.end(); ++medge) {
+            size_t facet_id1 = (*medge)[2];
+            size_t facet_id2 = (*medge)[3];
             if(facet_seen[facet_id1] && facet_seen[facet_id2]) {
-                edges[i].clear();
-                free_edges.push(i);
+                medge = edges.erase(medge);
+                --medge;
             }
             else if(facet_seen[facet_id1] || facet_seen[facet_id2]) {
-                size_t new_facet_id = hardcode_facet(edges[i][0], edges[i][1], curr_index, point_set);
-                edges[i][2] = new_facet_id;
-                edges[i][3] = facet_seen[facet_id2] ? facet_id1 : facet_id2;
+                size_t new_facet_id = hardcode_facet((*medge)[0], (*medge)[1], point_index, point_set);
+                (*medge)[2] = new_facet_id;
+                (*medge)[3] = facet_seen[facet_id2] ? facet_id1 : facet_id2;
                 for(size_t j = 0; j < 2; ++j) {
-                    if (temp.find(encode(curr_index, edges[i][j])) != temp.end()) {
-                        new_edges.emplace_back(vector<size_t>(
-                                {curr_index, edges[i][j], temp[encode(curr_index, edges[i][j])], new_facet_id}));
+                    if (temp.find(encode(point_index, (*medge)[j])) != temp.end()) {
+                        edges.emplace_front(vector<size_t>(
+                                {point_index, (*medge)[j], temp[encode(point_index, (*medge)[j])], new_facet_id}));
                     } else {
-                        temp[encode(curr_index, edges[i][j])] = new_facet_id;
+                        temp[encode(point_index, (*medge)[j])] = new_facet_id;
                     }
                 }
             }
-
-        }
-
-        for(const auto &vec : new_edges) {
-            hardcode_edge(vec[0], vec[1], vec[2], vec[3]);
         }
     }
 
@@ -115,35 +105,18 @@ private:
         hardcode_facet(2, 3, 0, point_set); // facet 2
         hardcode_facet(1, 2, 3, point_set); // facet 3
 
-        hardcode_edge(0, 1, 0, 1);
-        hardcode_edge(3, 0, 1, 2);
-        hardcode_edge(2, 3, 2, 3);
-        hardcode_edge(1, 2, 3, 0);
-        hardcode_edge(0, 2, 0, 2);
-        hardcode_edge(3, 1, 3, 1);
+        edges.emplace_back(vector<size_t>({0, 1, 0, 1}));
+        edges.emplace_back(vector<size_t>({3, 0, 1, 2}));
+        edges.emplace_back(vector<size_t>({2, 3, 2, 3}));
+        edges.emplace_back(vector<size_t>({1, 2, 3, 0}));
+        edges.emplace_back(vector<size_t>({0, 2, 0, 2}));
+        edges.emplace_back(vector<size_t>({3, 1, 3, 1}));
     }
 
     size_t hardcode_facet(size_t id1, size_t id2, size_t id3, const vector<point> &point_set) {
-        if(free_facets.empty()) {
-            facet.emplace_back(vector<size_t>({id1, id2, id3}));
-            sort_facet(facet.size() - 1, point_set);
-            return facet.size() - 1;
-        }
-        size_t free_place = free_facets.front();
-        free_facets.pop();
-        facet[free_place] = {id1, id2, id3};
-        sort_facet(free_place, point_set);
-        return free_place;
-    }
-
-    void hardcode_edge(size_t id1, size_t id2, size_t facet_id1, size_t facet_id2) {
-        if(free_edges.empty()) {
-            edges.emplace_back(vector<size_t>({id1, id2, facet_id1, facet_id2}));
-            return;
-        }
-        size_t free_place = free_edges.front();
-        free_edges.pop();
-        edges[free_place] = {id1, id2, facet_id1, facet_id2};
+        facet.emplace_back(std::make_pair(vector<size_t>({id1, id2, id3}), facet_index));
+        sort_facet(facet.back().first, point_set);
+        return facet_index++;
     }
 
     size_t encode(size_t a, size_t b) const {
@@ -154,10 +127,10 @@ private:
     }
 
 private:
-    vector<vector<size_t> > facet, edges;
-    std::queue<size_t> free_facets, free_edges;
+    std::list<std::pair<vector<size_t>, size_t> > facet;
+    std::list<vector<size_t> > edges;
     point super_point;
-    size_t curr_index;
+    size_t point_index = 0, facet_index = 0;
 };
 
 int main() {
