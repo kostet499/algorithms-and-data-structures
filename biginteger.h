47f83e6410bef5469eb75599a3b2f99b5d1b5d4c
coolhatsker <kostet499@gmail.com>
2018-11-26 15:02:20 +0300

    String representation problems

--- biginteger.h
+++ biginteger.h
@@ -5,6 +5,7 @@
 #ifndef BIGINTEGER_BIGINTEGER_H
 #define BIGINTEGER_BIGINTEGER_H
 
+using val_t = unsigned long long;
 class BigInteger {
 public:
     BigInteger(const BigInteger &other);
@@ -15,7 +16,7 @@ public:
 
     BigInteger&operator=(BigInteger other);
 
-    BigInteger(const int other);
+    BigInteger(int other);
 
     bool operator==(const BigInteger &other) const;
 
@@ -29,16 +30,97 @@ public:
 
     bool operator>=(const BigInteger &other) const;
 
+    bool LessAbs(const BigInteger &other) const;
+
+    BigInteger operator+(const BigInteger &other) const;
+
+    BigInteger operator-(const BigInteger &other) const;
+
+    void operator++();
+
+    void operator--();
+
+    std::string toString() const;
+
+    friend std::ostream&operator<<(std::ostream &os, const BigInteger &other);
+
+    friend std::istream&operator>>(std::istream &is, BigInteger &other);
 private:
-    static size_t power;
-    std::vector<unsigned long long> digit;
+    void sum(const std::vector<val_t> &a, const std::vector<val_t> &b, std::vector<val_t> &res) const;
+
+    void dif(const std::vector<val_t> &a, const std::vector<val_t> &b, std::vector<val_t> &res) const;
+
+    std::string ValToString(val_t value) const;
+private:
+    static short power;
+    static val_t modder;
+    std::vector<val_t> digit;
     bool sign;
 };
 
-size_t BigInteger::power = 32;
+short BigInteger::power = 10;
+val_t BigInteger::modder = 1e10;
+
+void BigInteger::sum(const std::vector<val_t> &a,
+                                                const std::vector<val_t> &b, std::vector<val_t> &res) const {
+    res.resize(std::max(b.size(), a.size()));
+    val_t lefting = 0;
+    for(size_t i = 0; i < res.size(); ++i) {
+        val_t current_digit = lefting;
+        if(i < a.size()) {
+            current_digit += a[i];
+        }
+        if(i < b.size()) {
+            current_digit += b[i];
+        }
+        lefting = current_digit / modder;
+        res[i] = current_digit % modder;
+    }
+    if(lefting > 0) {
+        res.emplace_back(lefting);
+    }
+}
+
+void BigInteger::dif(const std::vector<val_t> &a, const std::vector<val_t> &b, std::vector<val_t> &res) const {
+    res.resize(std::max(b.size(), a.size()), 0);
+    val_t lefting = 0;
+    size_t last_not_zero = 0;
+    for(size_t i = 0; i < res.size(); ++i) {
+        val_t have = i < a.size() ? a[i] : 0;
+        val_t need = i < b.size() ? b[i] + lefting : lefting;
+        if(have >= need) {
+            lefting = 0;
+            res[i] = have - need;
+        }
+        else {
+            lefting = 1;
+            res[i] = (have + modder) - need;
+        }
+        if(res[i] > 0) {
+            last_not_zero = i;
+        }
+    }
+    res.resize(last_not_zero + 1);
+}
+
+std::string BigInteger::ValToString(val_t value) const {
+    std::string res;
+    if(value == 0) {
+        return "0";
+    }
+    while(value > 0) {
+        res += static_cast<char>(value % 10 + static_cast<int>('0'));
+        value /= 10;
+    }
+    for(size_t i = 0; i < (res.size() >> 1); ++i) {
+        std::swap(res[i], res[res.size() - i - 1]);
+    }
+    return res;
+}
 
 BigInteger::BigInteger(const BigInteger &other) {
     digit = other.digit;
+    sign = other.sign;
 }
 
 BigInteger::BigInteger(BigInteger &&other) noexcept {
@@ -53,6 +135,7 @@ BigInteger& BigInteger::operator=(BigInteger other) {
 void swap(BigInteger &a, BigInteger &b) {
     using std::swap;
     swap(a.digit, b.digit);
+    swap(a.sign, b.sign);
 }
 
 BigInteger::BigInteger(const int other) {
@@ -81,6 +164,22 @@ bool BigInteger::operator<(const BigInteger &other) const {
         return false;
     }
 
+    return this->LessAbs(other);
+}
+
+bool BigInteger::operator>(const BigInteger &other) const {
+    return other < *this;
+}
+
+bool BigInteger::operator<=(const BigInteger &other) const {
+    return !(*this > other);
+}
+
+bool BigInteger::operator>=(const BigInteger &other) const {
+    return other <= *this;
+}
+
+bool BigInteger::LessAbs(const BigInteger &other) const {
     if(digit.size() < other.digit.size()) {
         return true;
     }
@@ -98,38 +197,65 @@ bool BigInteger::operator<(const BigInteger &other) const {
     return false;
 }
 
-bool BigInteger::operator>(const BigInteger &other) const {
-    return other < *this;
+BigInteger BigInteger::operator+(const BigInteger &other) const {
+    BigInteger biggy(0);
+    if(sign == other.sign) {
+        biggy.sign = sign;
+        sum(digit, other.digit, biggy.digit);
+        return biggy;
+    }
+
+    if(this->LessAbs(other)) {
+        biggy.sign = other.sign;
+        dif(other.digit, digit, biggy.digit);
+    }
+    else {
+        biggy.sign = sign;
+        dif(digit, other.digit, biggy.digit);
+    }
+
+    if(biggy.digit.back() == 0) {
+        biggy.sign = true;
+    }
+    return biggy;
 }
 
-bool BigInteger::operator<=(const BigInteger &other) const {
-    if(!sign && other.sign) {
-        return true;
+BigInteger BigInteger::operator-(const BigInteger &other) const {
+    BigInteger biggy(0);
+    if(sign ^ other.sign) {
+        biggy.sign = sign;
+        sum(digit, other.digit, biggy.digit);
+        return biggy;
     }
 
-    if(sign && !other.sign) {
-        return false;
+    if(this->LessAbs(other)) {
+        biggy.sign = !other.sign;
+        dif(other.digit, digit, biggy.digit);
+    }
+    else {
+        biggy.sign = sign;
+        dif(digit, other.digit, biggy.digit);
     }
 
-    if(digit.size() < other.digit.size()) {
-        return true;
+    if(biggy.digit.back() == 0) {
+        biggy.sign = true;
     }
-
-    if(digit.size() > other.digit.size()) {
-        return false;
-    }
-
-    for(size_t i = 0; i < digit.size(); ++i) {
-        if(digit[i] < other.digit[i]) {
-            return true;
-        }
-    }
-
-    return true;
+    return biggy;
 }
 
-bool BigInteger::operator>=(const BigInteger &other) const {
-    return other <= *this;
+std::string BigInteger::toString() const {
+    std::string res;
+    if(!sign) {
+        res += '-';
+    }
+    for(auto i = static_cast<int>(digit.size() - 1); i > -1; --i) {
+        res += ValToString(digit[i]);
+    }
+    return res;
 }
 
+std::ostream&operator<<(std::ostream &os, const BigInteger &other) {
+    os << other.toString();
+    return os;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "biginteger.h"
 
 int main() {
-
+    BigInteger k(2000000000);
+    BigInteger s(2000000000);
+    std::cout << k + k + k + k + k;
     return 0;
 }
