cb1f6dd9c66c20bd97255212ee26fdb44f853c56
coolhatsker <kostet499@gmail.com>
2018-12-07 05:34:51 +0300

    Refactoring

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void z_function(const string &pattern, vector<unsigned int> &z_values) {
+void z_function(const string &pattern, vector<size_t > &z_values) {
     z_values.resize(pattern.length(), 0);
     size_t left = 0;
     size_t right = 0;
@@ -25,8 +25,8 @@ void z_function(const string &pattern, vector<unsigned int> &z_values) {
     }
 }
 
-void prefix_func(const string &pattern, vector<unsigned int> &prefix_values) {
-    prefix_values.resize(pattern.length(), 0);
+void prefix_func(const string &pattern, vector<size_t> &prefix_values) {
+    prefix_values.resize(pattern.length());
     for(size_t i = 1; i < prefix_values.size(); ++i) {
         size_t j = prefix_values[i - 1];
         // jumping back on prefixes of prefixes of prefixes etc.
@@ -41,34 +41,32 @@ void prefix_func(const string &pattern, vector<unsigned int> &prefix_values) {
 }
 
 // Transforms zet function  to prefix function.
-vector<size_t > z_to_p(const vector<size_t > &z_values) {
-    vector<size_t > prefix_values(z_values.size(), 0);
+void z_to_p(const vector<size_t> &z_values, vector<size_t> &prefix_values) {
+    prefix_values.resize(z_values.size());
     for(size_t i = 1, prefix_position = 0; i < z_values.size(); ++i) {
         prefix_position = i + max(static_cast<int>(z_values[i]) - 1, 0);
         prefix_values[prefix_position] = max(prefix_values[prefix_position], z_values[i]);
     }
     int current_maximum = 0;
-    for(size_t i = prefix_values.size() - 1; i > - 1; --i) {
+    for(int i = static_cast<int>(prefix_values.size()) - 1; i > - 1; --i) {
         current_maximum = max(static_cast<int>(prefix_values[i]), current_maximum);
         prefix_values[i] = static_cast<size_t >(current_maximum);
         --current_maximum;
     }
-    return prefix_values;
 }
 
 // Transforms prefix function to zet function.
-vector<size_t > p_to_z(const vector<size_t > &prefix_values) {
-    vector<size_t> z_values(prefix_values.size(), 0);
-    for(int i = 0, z_position = 0; i < prefix_values.size(); --i) {
+void p_to_z(const vector<size_t > &prefix_values, vector<size_t> &z_values) {
+    z_values.resize(prefix_values.size());
+    for(size_t i = 0, z_position = 0; i < prefix_values.size(); ++i) {
         z_position = i - max(static_cast<int>(prefix_values[i]) - 1, 0);
         z_values[z_position] = max(z_values[z_position], prefix_values[i]);
     }
-    return z_values;
 }
 
 // Returns the value of prefix_function for symbol 'c' if 'last_prefix' is the last prefix function value.
-int local_prefix_func(const string &pattern, const vector<size_t > &prefix_values, char c, int last_prefix) {
-    while(last_prefix && c != pattern[last_prefix]) {
+size_t local_prefix_func(const string &pattern, const vector<size_t > &prefix_values, char c, size_t last_prefix) {
+    while(last_prefix > 0 && c != pattern[last_prefix]) {
         last_prefix = prefix_values[last_prefix - 1];
     }
 
@@ -87,7 +85,7 @@ string min_lex(const vector<size_t > &prefix_values) {
     string minimal_string = "a";
     for(size_t i = 1; i < prefix_values.size(); ++i) {
         for(size_t j = 0; j < 26; ++j) {
-            char c = 'a' + j;
+            char c = static_cast<char>(static_cast<int>('a') + j);
             if(local_prefix_func(minimal_string, prefix_values, c, prefix_values[i - 1]) == prefix_values[i]) {
                 minimal_string += c;
                 break;
@@ -103,7 +101,8 @@ int main() {
     while(cin >> x && x != -1) {
         z_values.emplace_back(x);
     }
-    z_values = z_to_p(z_values);
-    cout << min_lex(z_values);
+    vector<size_t> kke;
+    z_to_p(z_values, kke);
+    cout << min_lex(kke);
     return 0;
 }
