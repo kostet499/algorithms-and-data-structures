fd4068c40cbc6faff34e5a61f6223548e027c450
coolhatsker <kostet499@gmail.com>
2018-12-21 00:29:30 +0300

    refactoredX2

--- main.cpp
+++ main.cpp
@@ -4,41 +4,46 @@
 #include <algorithm>
 
 using namespace std;
-struct point {
-    double x, y, z;
-    point(double v1, double v2, double v3) : x(v1), y(v2), z(v3){}
-    point operator+(const point &b) const {
+class Point {
+public:
+    Point(double v1, double v2, double v3) : x(v1), y(v2), z(v3){}
+    Point operator+(const Point &b) const {
         return {x + b.x, y + b.y, z + b.z};
     }
-    point operator*(double number) const {
+    Point operator*(double number) const {
         return {x * number, y * number, z * number};
     }
-    point operator[](const point &b) const {
+    Point operator[](const Point &b) const {
         return {y * b.z - z * b.y, - x * b.z + z * b.x, x * b.y - y * b.x};
     }
-    point operator-(const point &b) const {
+    Point operator-(const Point &b) const {
         return {x - b.x, y - b.y, z - b.z};
     }
-    double operator()(const point &b) const {
+    double operator()(const Point &b) const {
         return x * b.x + y * b.y + z * b.z;
     }
     double operator()() const {
         return operator()(*this);
     }
+public:
+    static double comparing_precision;
+private:
+    double x, y, z;
 };
+double Point::comparing_precision = 1e-10;
 
-double angle(const point &a, const point &b) {
+double angle(const Point &a, const Point &b) {
     return a(b) / sqrt(a()) / sqrt(b());
 }
 
-double compute_distance(const point &dot, const point &a, const point &b) {
+double compute_distance(const Point &dot, const Point &a, const Point &b) {
     auto direction = b - a;
     auto touch = dot - a;
 
     auto angle_cos = angle(touch, direction);
     auto parameter = touch(direction) / direction();
 
-    if(direction() < 1e-8 || 0.0 > parameter) {
+    if(direction() < Point::comparing_precision || 0.0 > parameter) {
         auto difference = a - dot;
         return sqrt(difference());
     }
@@ -48,7 +53,7 @@ double compute_distance(const point &dot, const point &a, const point &b) {
         return sqrt(difference());
     }
 
-    // our point is on the line
+    // our Point is on the line
     // returns length of touch * sin(angle between touch and direction)
     double sq_cos = pow(angle_cos, 2);
     double sinus = sq_cos > 1 ? 0 : sqrt(1 - sq_cos);
@@ -56,18 +61,17 @@ double compute_distance(const point &dot, const point &a, const point &b) {
     return sqrt(touch()) * sinus;
 }
 
-double result_distance(const vector<point> &coord) {
+double result_distance(const vector<Point> &coord) {
     double mid = 0.0;
     double left = 0.0;
     double right = 1.0;
-    double compare_precision = 1e-10;
     auto direction = coord[1] - coord[0];
 
-    while(right - left > compare_precision) {
+    while(right - left > Point::comparing_precision) {
         mid = (left + right) / 2;
 
         auto fst = coord[0] + direction * mid;
-        auto scd = fst + direction * compare_precision;
+        auto scd = fst + direction * Point::comparing_precision;
 
         auto distance_difference = compute_distance(fst, coord[2], coord[3]) - compute_distance(scd, coord[2], coord[3]);
         if(distance_difference < 0) {
@@ -81,11 +85,11 @@ double result_distance(const vector<point> &coord) {
 }
 
 int main() {
-    vector<point> coord(4, point(0, 0, 0));
+    vector<Point> coord(4, Point(0, 0, 0));
     for(int i = 0; i < 4; ++i) {
         double x, y, z;
         cin >> x >> y >> z;
-        coord[i] = point(x, y, z);
+        coord[i] = Point(x, y, z);
     }
     printf("%0.7f", result_distance(coord));
     return 0;
